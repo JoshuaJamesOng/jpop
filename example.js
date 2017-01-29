827ac20ab3f843f820d9bf546738598f31f26faa
Ong, Joshua <Joshua.Ong@bskyb.com>
2017-01-29 22:51:57 +0000

    Make pop return a promise

--- example.js
+++ example.js
@@ -1,5 +1,15 @@
 'use strict';
 
 const JPOP = require('./src/index.js');
+const EXEC = require('child_process').exec;
 
-JPOP.pop({pwd: __dirname});
+JPOP.pop({pwd: __dirname}).then(function () {
+    EXEC('cat examples/output/examples/overrides/override.json', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+    });
+});
